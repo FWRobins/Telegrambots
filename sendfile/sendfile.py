57a6662f039c9dd403e398eeabe94e194593a610
FWRobins <fw.robins@gmail.com>
2021-03-07 09:36:23 +0200

    sendfile added

---
+++ sendfile/sendfile.py
@@ -0,0 +1,25 @@
+##I made this so I can quickly send a file to my cellphone
+##without having to emeil or log into whatappweb ect
+
+
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import telegram
+import time
+import pyautogui
+
+my_token = # removed for security purposed
+chatid = # chat id to whom to send the file
+
+##get file name from user
+myfile =  pyautogui.prompt(text='Transfer name?', title='Card Name', default='')
+myfile = myfile[1:-1]
+
+def send(myfile, chat_id, token=my_token):
+	bot = telegram.Bot(token=token)
+	bot.sendDocument(chat_id=chat_id, document=open(myfile, 'rb'), timeout=100000)
+
+
+send(myfile, chatid)
+
